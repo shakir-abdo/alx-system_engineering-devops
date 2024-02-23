e982b41418da5e8cc1d85a42630bf13bb27aa412
shakir abdo <shicolare1@gmail.com>
2024-02-23 22:11:58 +0300

    fix  1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 #!/usr/bin/pup
-# Using Puppet to install an specific version of flask = (2.1.0)
+# Using Puppt to install flask from pip3
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'

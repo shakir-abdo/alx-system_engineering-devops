0ced0935f4bfffc1b579026946ae2a70155b4ddd
Shakir Abdo <shicolare1@gmail.com>
2024-04-09 20:47:40 +0300

    fix task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,18 @@
-#!/usr/bin/python3
 """
-The number_of_subscribers function
+Returns the number of subs of a reddit sub
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for the given subreddit"""
-    if subreddit is None or not isinstance(subreddit, str):
+    """returns the number of subscribers for a given subreddit"""
+    headers = {"User-Agent": "Mozilla/5.0, ALX project"}
+
+    if not subreddit:
         return 0
-    r = requests.get(
-        "http://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Alx Project"},
-    ).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    return response.json()["data"]["subscribers"]

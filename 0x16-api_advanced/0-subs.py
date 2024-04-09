ba48e4355d3f151b821f77bf63b371d59bde992c
Shakir Abdo <shicolare1@gmail.com>
2024-04-09 20:45:03 +0300

    fix task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,6 +12,7 @@ def number_of_subscribers(subreddit):
         return 0
     r = requests.get(
         "http://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Alx Project"}).json()
+        headers={"User-Agent": "Alx Project"},
+    ).json()
     subs = r.get("data", {}).get("subscribers", 0)
     return subs

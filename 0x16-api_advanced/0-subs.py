38320ed0b71873002ea77b43c0f4f3d3d1d43ea1
Shakir Abdo <shicolare1@gmail.com>
2024-04-09 23:54:35 +0300

    fix task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {"User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10"}
+    user_agent = {"User-agent": "Mozilla/5.0"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = get(url, headers=user_agent)
     results = response.json()
